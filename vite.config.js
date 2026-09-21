41708b363a01e26ccd96199400feeca1befc0788
sonwon-03 <sekown2003@gmail.com>
2026-09-21 22:58:59 +0900

    up3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/w26w03-react-counter/',
+  base: '/w26w03-react-counter/', // 이 부분이 핵심입니다!
 })
